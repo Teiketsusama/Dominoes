edf70f288ef2129ac0f86ae5c2cdde5fa59a1203
june <djie627@gmail.com>
2023-02-03 14:05:44 +1100

    Stage 2/5: The Interface

--- dominoes.py
+++ dominoes.py
@@ -25,47 +25,6 @@ def split_domino(random_set):
     return random_set, player1, player2
 
 
-# The code below is for single highest domino
-#
-#
-# def best_starting_piece(lst):
-#     best_index = 0
-#     best_piece = lst[0]
-#     for i in range(1, len(lst)):
-#         compare_piece = lst[i]
-#         best_max = max(best_piece)
-#         compare_max = max(compare_piece)
-#         if compare_max > best_max:
-#             best_index = i
-#             best_piece = compare_piece
-#         elif compare_max == best_max:
-#             best_sum = sum(best_piece)
-#             compare_sum = sum(compare_piece)
-#             if compare_sum > best_sum:
-#                 best_index = i
-#                 best_piece = compare_piece
-#     return best_index, best_piece
-#
-#
-# def player_status(piece1, piece2):
-#     chosen_piece = []
-#     counter = 0
-#     if max(piece1) > max(piece2):
-#         chosen_piece = piece1
-#         counter += 1
-#     elif max(piece1) == max(piece2):
-#         sum1 = sum(piece1)
-#         sum2 = sum(piece2)
-#         if sum1 > sum2:
-#             chosen_piece = piece1
-#             counter += 1
-#         elif sum1 < sum2:
-#             chosen_piece = piece2
-#     elif max(piece1) < max(piece2):
-#         chosen_piece = piece2
-#     return counter, chosen_piece
-
-
 def domino_double(lst):
     lst_double = []
     for sublist in lst:
@@ -102,29 +61,43 @@ def player_status(piece1, piece2):
     return counter, chosen_piece
 
 
-while True:
-    domino_set = generate_domino()
-    stock_pieces, computer_pieces, player_pieces = split_domino(domino_set)
+def print_header():
+    header = 70 * "="
+    print(header)
 
-    computer_double = domino_double(computer_pieces)
-    player_double = domino_double(player_pieces)
 
-    if computer_double is not False or player_double is not False:
-        break
+if __name__ == "__main__":
+    while True:
+        domino_set = generate_domino()
+        stock_pieces, computer_pieces, player_pieces = split_domino(domino_set)
 
-computer_best_index, computer_best_piece = best_starting_piece(computer_pieces)
-player_best_index, player_best_piece = best_starting_piece(player_pieces)
+        computer_double = domino_double(computer_pieces)
+        player_double = domino_double(player_pieces)
 
-status = ["computer", "player"]
-status_index, max_piece = player_status(computer_best_piece, player_best_piece)
-if status_index == 1:
-    computer_pieces.remove(max_piece)
-elif status_index == 0:
-    player_pieces.remove(max_piece)
-max_piece = [max_piece]
+        if computer_double is not False or player_double is not False:
+            break
 
-print("Stock pieces: {}".format(stock_pieces))
-print("Computer pieces: {}".format(computer_pieces))
-print("Player pieces: {}".format(player_pieces))
-print("Domino snake: {}".format(max_piece))
-print("Status: {}".format(status[status_index]))
+    computer_best_index, computer_best_piece = best_starting_piece(computer_pieces)
+    player_best_index, player_best_piece = best_starting_piece(player_pieces)
+
+    status = ["Computer is about to make a move. Press Enter to continue...",
+              "It's your turn to make a move. Enter your command."]
+    status_index, max_piece = player_status(computer_best_piece, player_best_piece)
+    if status_index == 1:
+        computer_pieces.remove(max_piece)
+    elif status_index == 0:
+        player_pieces.remove(max_piece)
+
+    # Display
+    print_header()
+    print("Stock size: {}".format(len(stock_pieces)))
+    print("Computer pieces: {}".format(len(computer_pieces)))
+    print()
+    print(max_piece)
+    print()
+    print("Your pieces:")
+    i = 0
+    for player_piece in player_pieces:
+        i += 1
+        print("{}:{}".format(i, player_piece))
+    print("Status: {}".format(status[status_index]))
